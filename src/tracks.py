cdd55d06492401afb86c98a90a0ffee5426df05e
Jordan Reed <jordanreed@Jordans-Air.lan>
2023-01-05 20:00:23 -0500

    get track data

--- src/tracks.py
+++ src/tracks.py
@@ -6,20 +6,48 @@ import os
 
 from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyClientCredentials
+from logging import raiseExceptions
 
 load_dotenv() #load envirnoment vars from .env files
 
 CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
 CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
+PLAYLIST_ID = "1BXj2SlxJLcXSy07Fv9Bgg"
+
+"""
+Function that extracts track data from a playlist
+
+@param playlistid - id of playlist
+@return tracks - list of data for each track
+
+"""
+def get_track_data(playlistId):
+    try:
+        auth_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+        sp = spotipy.Spotify(auth_manager=auth_manager)
+        playlist = sp.playlist_tracks(playlistId)
+
+    except spotipy.SpotifyException as e:
+        raise Exception(e)
+
+    # extract useful attributes
+    seenAlbums = [] #ensures that we don't get duplicate images
+    tracks = []
+    for item in playlist["items"]:
+        albumId = item["track"]["album"]["id"]
+        if albumId not in seenAlbums:
+            track = {}
+            track["song_id"] = item["track"]["id"]
+            track["track_image"] = item["track"]["album"]["images"][2]["url"]
+            tracks.append(track)
+            seenAlbums.append(albumId)
+
+    return tracks
 
 def main():
-    
-    print(CLIENT_ID, CLIENT_SECRET)
-    print(os.environ.get('USER'))
-    auth_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
-    sp = spotipy.Spotify(auth_manager=auth_manager)
-    print(sp)
-    
+    track_data = get_track_data(PLAYLIST_ID)
+    print(len(track_data))
+
 
 if __name__=="__main__":
     main()
