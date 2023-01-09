94ae24e582d04a27ea5518f186820fb129e23a8f
Jordan Reed <jordanreed@Jordans-Air.lan>
2023-01-09 18:42:06 -0500

    closest color function

--- src/Mosaify.py
+++ src/Mosaify.py
@@ -1,6 +1,7 @@
 """
 File that will get the tracks from a Spotify playlist
 """
+from curses import color_content
 import cv2
 import numpy as np
 import requests
@@ -51,6 +52,7 @@ def get_track_data(playlistId):
 
 def get_images_dominant_color(trackData):
     image_objects = []
+    colors = []
     for image in trackData:
         
         try:
@@ -59,7 +61,7 @@ def get_images_dominant_color(trackData):
             npArr = np.frombuffer(res.content, np.uint8)
             imgArr = cv2.imdecode(npArr, cv2.IMREAD_UNCHANGED)
 
-            color = most_dominent_coler(imgArr)
+            color = most_dominent_color(imgArr)
 
             imgData = {
                 "color" : color,
@@ -67,15 +69,16 @@ def get_images_dominant_color(trackData):
             }
 
             image_objects.append(imgData)
+            colors.append(color)
 
         except requests.exceptions.RequestException as e:
             # print("Call to Spotify to download track image failed: Ensure id is correct")
             raise e
             
-    return image_objects
+    return image_objects, colors
     
 
-def most_dominent_coler(imgArr, size=(16, 16), k=4):
+def most_dominent_color(imgArr, size=(16, 16), k=4):
     # resize image for faster processing
     image = cv2.resize(imgArr, size, 
                             interpolation = cv2.INTER_AREA)
@@ -99,6 +102,17 @@ def most_dominent_coler(imgArr, size=(16, 16), k=4):
 
     return rounded
 
+"""
+Function that finds the closest color in a list of colors
+"""
+def closest_color(colors, color):
+    colors = np.array(colors)
+    color = np.array(color)
+    distances = np.sqrt(np.sum((colors-color)**2,axis=1))
+    index_of_smallest = np.where(distances==np.amin(distances))
+    smallest_distance = colors[index_of_smallest]
+    return index_of_smallest[0][0], smallest_distance 
+
 def save_image(filename, imgArr):
     cv2.imwrite(filename, imgArr)
 
@@ -111,9 +125,34 @@ def resize_image(imgArr, size=(16, 16)):
 
 def main():
     track_data = get_track_data(PLAYLIST_ID)
-    print(len(track_data))
+    # print(len(track_data))
 
-    imageData = get_images_dominant_color(track_data)
+    imageData, colors = get_images_dominant_color(track_data)
+
+    print(colors)
+    list_of_colors = colors
+    # list_of_colors = [[52,211,235],[150,33,77],[75,99,23],[45,88,250],[250,0,255]]
+    # color = [52,189,235]
+    # closetColor = closest_color(list_of_colors, color)
+    # print(closetColor)
+
+    # image = np.zeros((16, 16, 3), np.uint8)
+    # image[:] = color
+    # save_image("./images/closest-original.png", image)
+
+    # image[:] = closetColor
+    # save_image("./images/closest-color.png", image)
+
+    path = "./images/beach-255x198.jpeg"
+    img = cv2.imread(path)
+    # print(img)
+
+    
+    color = [0,0,0] #img[0][0]
+    index, closetColor = closest_color(list_of_colors, color)
+    print(color)
+    print(closetColor[0])
+    print(list_of_colors[index])
 
 
 if __name__=="__main__":
