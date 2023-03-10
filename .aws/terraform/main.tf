5de0594a2f35e9d8341fd9a7a8d0ea7fb66f1a39
Jordan Reed <jordan.reed.aws@gmail.com>
2023-03-10 12:25:48 -0500

    add src and update tf

--- .aws/terraform/main.tf
+++ .aws/terraform/main.tf
@@ -9,3 +9,23 @@ resource "aws_s3_bucket_website_configuration" "website_bucket" {
     suffix = "index.html"
   }
 }
+
+resource "aws_s3_bucket_policy" "allow_public_access" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = data.aws_iam_policy_document.allow_public_access.json
+}
+
+data "aws_iam_policy_document" "allow_public_access" {
+  statement {
+    effect = "Allow"
+    actions = "s3:GetObject"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    resources = [
+      aws_s3_bucket.bucket.arn,
+      "${aws_s3_bucket.bucket.arn}/*",
+    ]
+  }
+}
