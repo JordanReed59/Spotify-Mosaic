04e0d32681a3332fb2d29ea16558673ca8de5309
Jordan Reed <jordan.reed.aws@gmail.com>
2023-03-13 15:42:58 -0400

    add api method

--- .aws/terraform/main.tf
+++ .aws/terraform/main.tf
@@ -88,8 +88,8 @@ resource "aws_api_gateway_method" "post_method" {
 
 resource "aws_api_gateway_integration" "root_lambda" {
   rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
-  resource_id = "${aws_api_gateway_method.get_method.root_resource_id}"
-  http_method = "${aws_api_gateway_method.get_method.http_method}"
+  resource_id = "${aws_api_gateway_method.post_method.resource_id}"
+  http_method = "${aws_api_gateway_method.post_method.http_method}"
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
