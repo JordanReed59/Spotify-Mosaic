de8c8f88fcfa074c4c27e7bb4c06bd03b4a0bd22
Jordan Reed <jordan.reed.aws@gmail.com>
2023-03-13 15:19:50 -0400

    update api method

--- .aws/terraform/main.tf
+++ .aws/terraform/main.tf
@@ -65,7 +65,7 @@ resource "aws_api_gateway_resource" "get_method_resource" {
 resource "aws_api_gateway_method" "get_method" {
   rest_api_id   = aws_api_gateway_rest_api.gateway.id
   resource_id   = aws_api_gateway_resource.get_method_resource.id
-  http_method   = "POST"
+  http_method   = "GET"
   authorization = "NONE"
 }
 
