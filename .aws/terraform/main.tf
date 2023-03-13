0af2e43b3c5469bdfb59b9baaca858fec2b60bf7
Jordan Reed <jordan.reed.aws@gmail.com>
2023-03-13 15:47:43 -0400

    add api method

--- .aws/terraform/main.tf
+++ .aws/terraform/main.tf
@@ -79,27 +79,27 @@ resource "aws_api_gateway_integration" "lambda" {
   uri                     = "${aws_lambda_function.mosaify_backend.invoke_arn}"
 }
 
-resource "aws_api_gateway_method" "post_method" {
-  rest_api_id   = aws_api_gateway_rest_api.gateway.id
-  resource_id   = aws_api_gateway_rest_api.gateway.root_resource_id
-  http_method   = "POST"
-  authorization = "NONE"
-}
+# resource "aws_api_gateway_method" "post_method" {
+#   rest_api_id   = aws_api_gateway_rest_api.gateway.id
+#   resource_id   = aws_api_gateway_rest_api.gateway.root_resource_id
+#   http_method   = "POST"
+#   authorization = "NONE"
+# }
 
-resource "aws_api_gateway_integration" "root_lambda" {
-  rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
-  resource_id = "${aws_api_gateway_method.post_method.resource_id}"
-  http_method = "${aws_api_gateway_method.post_method.http_method}"
+# resource "aws_api_gateway_integration" "root_lambda" {
+#   rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
+#   resource_id = "${aws_api_gateway_method.post_method.resource_id}"
+#   http_method = "${aws_api_gateway_method.post_method.http_method}"
 
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.mosaify_backend.invoke_arn}"
-}
+#   integration_http_method = "POST"
+#   type                    = "AWS_PROXY"
+#   uri                     = "${aws_lambda_function.mosaify_backend.invoke_arn}"
+# }
 
 resource "aws_api_gateway_deployment" "deploy_api" {
   depends_on = [
     "aws_api_gateway_integration.lambda",
-    "aws_api_gateway_integration.root_lambda"
+    # "aws_api_gateway_integration.root_lambda"
   ]
 
   rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
