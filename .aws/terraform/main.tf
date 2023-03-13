00bf882557456bb70dcaa283e63be01a5ccd0255
Jordan Reed <jordan.reed.aws@gmail.com>
2023-03-13 15:50:01 -0400

    add api method

--- .aws/terraform/main.tf
+++ .aws/terraform/main.tf
@@ -65,7 +65,7 @@ resource "aws_api_gateway_resource" "get_method_resource" {
 resource "aws_api_gateway_method" "get_method" {
   rest_api_id   = aws_api_gateway_rest_api.gateway.id
   resource_id   = aws_api_gateway_resource.get_method_resource.id
-  http_method   = "GET"
+  http_method   = "POST"
   authorization = "NONE"
 }
 
