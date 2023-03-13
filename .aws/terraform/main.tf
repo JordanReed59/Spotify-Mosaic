283b93f381ff1c6fc05d2cfc05a137c17dee6189
Jordan Reed <jordan.reed.aws@gmail.com>
2023-03-13 15:30:40 -0400

    add api method

--- .aws/terraform/main.tf
+++ .aws/terraform/main.tf
@@ -79,6 +79,23 @@ resource "aws_api_gateway_integration" "lambda" {
   uri                     = "${aws_lambda_function.mosaify_backend.invoke_arn}"
 }
 
+resource "aws_api_gateway_method" "post_method" {
+  rest_api_id   = aws_api_gateway_rest_api.gateway.id
+  resource_id   = aws_api_gateway_rest_api.gateway.root_resource_id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "root_lambda" {
+  rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
+  resource_id = "${aws_api_gateway_method.get_method.resource_id}"
+  http_method = "${aws_api_gateway_method.get_method.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.mosaify_backend.invoke_arn}"
+}
+
 resource "aws_api_gateway_deployment" "deploy_api" {
   depends_on = [
     "aws_api_gateway_integration.lambda"
