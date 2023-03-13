62c110af0c3cf14da9ba1bc4b7aaa224eb0b38c3
Jordan Reed <jordan.reed.aws@gmail.com>
2023-03-12 21:53:39 -0400

    api gateway config update

--- .aws/terraform/main.tf
+++ .aws/terraform/main.tf
@@ -48,24 +48,33 @@ resource "aws_api_gateway_rest_api" "gateway" {
 resource "aws_api_gateway_resource" "get_method_resource" {
   rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
   parent_id   = "${aws_api_gateway_rest_api.gateway.root_resource_id}"
-  path_part   = "{proxy+}"
+  path_part   = "mosaify"
 }
 
 resource "aws_api_gateway_method" "get_method" {
   rest_api_id   = aws_api_gateway_rest_api.gateway.id
-  resource_id   = aws_api_gateway_resource.MyDemoResource.id
+  resource_id   = aws_api_gateway_resource.get_method_resource.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
-esource "aws_api_gateway_integration" "lambda" {
+resource "aws_api_gateway_integration" "lambda" {
   rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
   resource_id = "${aws_api_gateway_method.get_method.resource_id}"
   http_method = "${aws_api_gateway_method.get_method.http_method}"
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.terraform_lambda_func.invoke_arn}"
+  uri                     = "${aws_lambda_function.mosaify_backend.invoke_arn}"
+}
+
+resource "aws_api_gateway_deployment" "example" {
+  depends_on = [
+    "aws_api_gateway_integration.lambda"
+  ]
+
+  rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
+  stage_name  = "test"
 }
 
 ########## Lambda configuration ##########
@@ -107,7 +116,7 @@ resource "aws_lambda_layer_version" "lambda_layer" {
 #   compatible_runtimes = ["python3.8"]
 #   source_code_hash    = "${filebase64sha256("python_layer.z02")}"
 # }
-resource "aws_lambda_function" "terraform_lambda_func" {
+resource "aws_lambda_function" "mosaify_backend" {
   filename                       = "${path.module}/python/mosaify.zip"
   function_name                  = "mosaify_backend"
   role                           = aws_iam_role.lambda_role.arn
@@ -122,7 +131,7 @@ resource "aws_lambda_function" "terraform_lambda_func" {
 resource "aws_lambda_permission" "lambda_permission" {
   statement_id  = "AllowMosaifyAPIInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.terraform_lambda_func.function_name
+  function_name = aws_lambda_function.mosaify_backend.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.gateway.execution_arn}/*/*"
 }
