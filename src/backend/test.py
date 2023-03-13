11d981b0b93f270083f8e12d6a808be179d8364f
Jordan Reed <jordan.reed.aws@gmail.com>
2023-03-12 22:11:02 -0400

    return response

--- src/backend/test.py
+++ src/backend/test.py
@@ -1,17 +1,26 @@
-import cv2
-import numpy as np
-import requests
-import spotipy
-import os
+# import cv2
+# import numpy as np
+# import requests
+# import spotipy
+# import os
 
 # from curses import color_content
-from dotenv import load_dotenv
-from spotipy.oauth2 import SpotifyClientCredentials
-from logging import raiseExceptions
-from sklearn.cluster import KMeans
-from collections import Counter
+# from dotenv import load_dotenv
+# from spotipy.oauth2 import SpotifyClientCredentials
+# from logging import raiseExceptions
+# from sklearn.cluster import KMeans
+# from collections import Counter
 
 def lambda_handler(event, context):
-    requests.get("https://www.google.com/search?q=hello")
-    print(event)
+    # requests.get("https://www.google.com/search?q=hello")
+    # print(event)
+    response = {
+        "statusCode": 200,
+        "headers": {
+            'Content-Type': 'text/html; charset=utf-8',
+        },
+        "body": "Hello world"
+    }
+
+    return response
     
