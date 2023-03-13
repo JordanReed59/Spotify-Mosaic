a110896e48c7d906b7b9462fa44d76d484d3c80a
Jordan Reed <jordan.reed.aws@gmail.com>
2023-03-12 21:08:12 -0400

    api gateway update

--- .aws/terraform/main.tf
+++ .aws/terraform/main.tf
@@ -45,6 +45,29 @@ resource "aws_api_gateway_rest_api" "gateway" {
   description = "Terraform Serverless API Gateway for Mosaify"
 }
 
+resource "aws_api_gateway_resource" "get_method_resource" {
+  rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
+  parent_id   = "${aws_api_gateway_rest_api.gateway.root_resource_id}"
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "get_method" {
+  rest_api_id   = aws_api_gateway_rest_api.gateway.id
+  resource_id   = aws_api_gateway_resource.MyDemoResource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+esource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
+  resource_id = "${aws_api_gateway_method.get_method.resource_id}"
+  http_method = "${aws_api_gateway_method.get_method.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.terraform_lambda_func.invoke_arn}"
+}
+
 ########## Lambda configuration ##########
 data "archive_file" "python_code_zip" {
   type        = "zip"
