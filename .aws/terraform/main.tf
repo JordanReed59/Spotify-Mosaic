1f400f388e3045dcdccb8d7613da0e8e21043a52
Jordan Reed <jordan.reed.aws@gmail.com>
2023-03-13 00:56:50 -0400

    s3 bucket cors config

--- .aws/terraform/main.tf
+++ .aws/terraform/main.tf
@@ -26,11 +26,6 @@ resource "aws_s3_bucket_cors_configuration" "example" {
     allowed_origins = ["*"]
     expose_headers  = []
   }
-
-  cors_rule {
-    allowed_methods = ["GET"]
-    allowed_origins = ["*"]
-  }
 }
 
 resource "aws_s3_bucket_policy" "allow_public_access" {
