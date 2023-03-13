e149cfa3d1ab5ee5eaf8bfed188e5000930e53ad
Jordan Reed <jordan.reed.aws@gmail.com>
2023-03-13 16:26:52 -0400

    add api method

--- .aws/terraform/main.tf
+++ .aws/terraform/main.tf
@@ -99,7 +99,7 @@ resource "aws_api_gateway_integration" "root_lambda" {
 resource "aws_api_gateway_deployment" "deploy_api" {
   depends_on = [
     "aws_api_gateway_integration.lambda",
-    # "aws_api_gateway_integration.root_lambda"
+    "aws_api_gateway_integration.root_lambda"
   ]
 
   rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
